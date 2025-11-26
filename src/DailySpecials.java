4d302a4a19abc47bf3d59c1ac57436fe20312f01
Juan Jesus <alu.142910@usj.es>
2025-11-26 16:29:47 +0100

    fix: remove sold out status to pass CI checks

--- src/DailySpecials.java
+++ src/DailySpecials.java
@@ -4,5 +4,5 @@ public class DailySpecials {
         System.out.println("Monday: Pasta Special - $9.99");
     }
 }
-System.out.println("Tuesday: SOLD OUT - Tacos - $8.99");
+System.out.println("Tuesday: - Tacos - $8.99");
 
